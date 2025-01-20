754e6d14ebe9bf6bc84ee3e2322f185365b698b8
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-20 16:52:10 -0600

    feat: FlexBetween for consistent and easy styling. outlet for routes. Navbar imports

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  MenuIcon as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "./FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import ProfileImage from "assets/profile.jpg";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
