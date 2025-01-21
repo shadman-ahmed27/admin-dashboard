971f13d6b22504b0996c54d2afb59d69664b5aab
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-20 19:27:35 -0600

    feat: created side bar component with collapse ability

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,19 +1,33 @@
-import { Box, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import { Box, useTheme, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+
 const Layout = () => {
   const theme = useTheme();
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
-    <Box
-      width="100%"
-      height="100%"
-      sx={{
-        backgroundColor: theme.palette.mode === "dark" ? "#181C3B" : "#ffffff",
-      }}
-    >
-      <Box>
-        <Navbar />
+    <Box display="flex" width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box
+        flexGrow={1}
+        sx={{
+          backgroundColor:
+            theme.palette.mode === "dark" ? "#181C3B" : "#ffffff",
+        }}
+      >
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
