883773afbf7a98edadcdb3b8a0001ef6353280af
Shad <Shadifmg@gmail.com>
2025-01-24 16:10:10 -0600

    Feat: Created the rest of the MongoDB schema with city, state, country, phone number, transactions and role with an enum of 3 diff user types that they can be, default admin

---
+++ server/modelsMongoDB/User.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 100,
+    },
+    city: String,
+    state: String,
+    country: String,
+    phoneNumer: String,
+    transactions: Array,
+    role: {
+      type: String,
+      enum: ["user", "admin", "superadmin"],
+      default: "admin",
+    },
+  },
+  { timestamps: true } //auto create and update a date
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
